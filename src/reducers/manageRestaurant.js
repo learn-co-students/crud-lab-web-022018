90191270a12cf29b569382e1c3ea0834309e7073
lidianvilazio <lidianvilazio@hotmail.com>
2018-05-11 13:28:13 -0400

    Done.

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -2,6 +2,30 @@
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
+let id = 0;
 
-}
+export default function manageRestaurants(state = {
+  restaurants: [],
+  reviews: []
+}, action) {
+  switch (action.type) {
+    case 'ADD_RESTAURANT':
+      id++
+      const restaurant = Object.assign({}, action.restaurant, { id: id })
+      return { restaurants: state.restaurants.concat(restaurant) }
+    case 'DELETE_RESTAURANT':
+      const restaurants = state.restaurants.filter(restaurant => restaurant.id !== action.id);
+      return { restaurants };
+    case 'ADD_REVIEW':
+      const review = Object.assign({}, action.review, { id: cuidFn() });
+      return Object.assign({}, state, {
+        reviews: state.reviews.concat(review),
+      });
+
+    case 'DELETE_REVIEW':
+      const reviews = state.reviews.filter(review => review.id !== action.id);
+      return Object.assign({}, state, { reviews });
+    default:
+      return state;
+  }
+};
